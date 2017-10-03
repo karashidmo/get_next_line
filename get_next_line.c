5f0089c2935e6db4c9524aa1e6c6a3e3fe6e5225
karashidmm <krashid-@student.42.fr>
2017-10-03 20:41:04 +0200

    new lines

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: krashid- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/27 22:53:39 by krashid-          #+#    #+#             */
-/*   Updated: 2017/10/03 19:40:39 by krashid-         ###   ########.fr       */
+/*   Updated: 2017/10/03 20:38:54 by krashid-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,43 +19,37 @@ int get_next_line(const int fd, char **line)
 {
 
 	char *buff;
-	static char ret;
+	static int ret;
+
+	buff = 0;
 	while (read(fd, buff, BUFF_SIZE) > 0)
 	{
 		while (BUFF_SIZE >= fd && line)
 		{
 			if (!(fd != '\n'))
-				buff[ret] = NULL;
+				buff[ret] = 0;
 			else if (fd == '\0')
 				return (1);
 			else if (fd == '\n')
-				buff[ret] = line;
+				buff = *line;
+			line++;
 		}
-		*line++;
 	}
 	return(0);
-}
-
-int searchn(const int fd,char *line)
-{
-	const char *str1;
-
-	str1 = line;
-	while(fd)
-		ft_strchr(str1, '\n' + 48);
-	return (1);
 }
 
 int main(int ac, char **av)
 {
 	int fd;
-	int i;
+//	int i;
 	char **line = NULL;
 
-	i = 0;
-	while(ac >+ 2 && (fd = open(av[i], O_RDONLY) > 0))
+	while(ac == 2 && (!(fd = open(av[1], O_RDONLY) > 0)))
 	{
+		if (!(open(av[1], O_RDONLY))) 
+			 printf("error");
 		get_next_line((const int) fd, line);
+		printf("line copied is %s \n", *line);
 	}
 	return(0);
 }
